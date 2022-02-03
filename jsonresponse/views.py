46479333dd10836ebb1d15a1330f09a6b496b59b
DS-jr <93123740+DS-jr@users.noreply.github.com>
2022-02-03 13:04:42 +0300

    Update views.py

--- jsonresponse/views.py
+++ jsonresponse/views.py
@@ -3,6 +3,6 @@ from django.http import JsonResponse
 def send_json(request):
 
     data = [{'name': "Ray Dalio", 'quoTe1': "Go to youR pAin & eXplore it"},
-            {'name': "LKY", "quOte2": "DeTerMinatioN creates a huge internal drive & focus"}]
+            {'name': "Lee Kuan Yew", "quOte2": "DeTerMinatioN creates a huge internal drive & focus. People feel if you are determined or not."}]
 
     return JsonResponse(data, safe=False)
